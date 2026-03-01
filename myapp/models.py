bc527ea2383288e4dbe9bb392a4235dc62a1c7ce
Gabriellungu123 <gabilungu913@gmail.com>
2026-03-01 12:12:26 +0100

    Hasta DJANGO SHELL

--- myapp/models.py
+++ myapp/models.py
@@ -3,4 +3,12 @@ from django.db import models
 # Create your models here.
 class Project(models.Model):
     name = models.CharField(max_length=200)
+
     
+    
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE) #Ese foreignkey es para relacionar esa tabla con la de arriba
+                                                                    #Ese on_delete=models.CASCADE es para que si la tabla anterior se borra lo que le siga a la relacionada tambien 
+  
