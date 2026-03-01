afd2552f03613851011834890865277fa439551e
Gabriellungu123 <gabilungu913@gmail.com>
2026-03-01 15:27:16 +0100

    Django Admin

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Project, Task
+from django.shortcuts import get_object_or_404
 
 
 def index(request):
@@ -16,3 +17,10 @@ def numero(request, id):
 def about(request):
     return HttpResponse("About")
 
+def projects(request):
+    projects = list(Project.objects.values())
+    return(JsonResponse(projects, safe=False))
+
+def tasks(request, id):
+    task = get_object_or_404(Task, id=id)
+    return(HttpResponse('tasks: %s' % task.title))
