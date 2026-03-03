5562d1214e344afe7cb2706c1cf61f19c4fb0844
Gabriellungu123 <gabilungu913@gmail.com>
2026-03-03 11:51:47 +0100

    Render

--- myapp/models.py
+++ myapp/models.py
@@ -4,11 +4,15 @@ from django.db import models
 class Project(models.Model):
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name #Esto es clave
+
     
     
 class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    project = models.ForeignKey(Project, on_delete=models.CASCADE) #Ese foreignkey es para relacionar esa tabla con la de arriba
-                                                                    #Ese on_delete=models.CASCADE es para que si la tabla anterior se borra lo que le siga a la relacionada tambien 
-  
+    project = models.ForeignKey(Project, on_delete=models.CASCADE) #Ese foreignkey es para relacionar esa tabla con la de arriba  #Ese on_delete=models.CASCADE es para que si la tabla anterior se borra lo que le siga a la relacionada tambien 
+    
+    def __str__(self):
+        return self.title #Esto es clave fijate el title que no es name
